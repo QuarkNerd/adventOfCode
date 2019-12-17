e28b5902a94a1b1751ddf4f20a15349c65420219
Gurveer Arora <Garora1410@yahoo.co.uk>
2019-12-17 21:41:02 +0000

    completed day 7 part 2

--- 7_Amplification.js
+++ 7_Amplification.js
@@ -1,10 +1,9 @@
 const input =
   "3,8,1001,8,10,8,105,1,0,0,21,34,51,64,81,102,183,264,345,426,99999,3,9,102,2,9,9,1001,9,4,9,4,9,99,3,9,101,4,9,9,102,5,9,9,1001,9,2,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,4,9,99,3,9,102,3,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,5,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99";
-// const input =
-//   "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
 const B = input.split(",");
 const intcode = B.map(entry => parseInt(entry));
-
+solvePartOne();
+solvePartTwo();
 function solvePartOne() {
   const allThrusterPhases = getAllPermutations([0, 1, 2, 3, 4]);
   let highestThrusterOutput = 0;
@@ -15,7 +14,17 @@ function solvePartOne() {
   });
   console.log(highestThrusterOutput);
 }
-solvePartOne();
+
+function solvePartTwo() {
+  const allThrusterPhases = getAllPermutations([9, 8, 7, 6, 5]);
+  let highestThrusterOutput = 0;
+  let output;
+  allThrusterPhases.forEach(thrusterPhase => {
+    output = getThrusterOutputInFeedbackMode([...intcode], thrusterPhase);
+    highestThrusterOutput = Math.max(output, highestThrusterOutput);
+  });
+  console.log(highestThrusterOutput);
+}
 
 function getThrusterOutputInNormalMode(intcode, phaseOrder) {
   let input = 0;
@@ -27,32 +36,38 @@ function getThrusterOutputInNormalMode(intcode, phaseOrder) {
   return input;
 }
 
-console.log(getThrusterOutputInFeedbackMode(intcode, [9, 8, 7, 6, 5]));
-
 function getThrusterOutputInFeedbackMode(intcode, phaseOrder) {
   const intcodesList = Array.apply(null, Array(5)).map(_ => [...intcode]);
   let input = 0;
   let i = 0;
+  const startingPos = [0, 0, 0, 0, 0];
   while (true) {
-    // console.log(phaseOrder[i % 5])
     result = computeIntcode(
       intcodesList[i % 5],
       phaseOrder[i % 5],
       input,
-      i > 4
+      i > 4,
+      startingPos[i % 5]
     );
     if (result.halted) {
       break;
     } else {
-      i++;
+      startingPos[i % 5] = result.i;
       input = result.value;
+      i++;
     }
   }
   return input;
 }
 
-function computeIntcode(C, phaseSetting, inputFromPrev, phaseGiven = false) {
-  let i = 0;
+function computeIntcode(
+  C,
+  phaseSetting,
+  inputFromPrev,
+  phaseGiven = false,
+  startingPos = 0
+) {
+  let i = startingPos;
   let opcode;
   let instruction;
   let parameterModes = { 1: undefined, 2: undefined };
@@ -79,13 +94,15 @@ function computeIntcode(C, phaseSetting, inputFromPrev, phaseGiven = false) {
         break;
       case 3:
         C[C[i + 1]] = phaseGiven ? inputFromPrev : phaseSetting;
-        // console.log(phaseGiven ? inputFromPrev : phaseSetting, phaseSetting)
         phaseGiven = true;
         i += 2;
         break;
       case 4:
-        console.log("output", getValue(C, i + 1, 0));
-        return { halted: false, value: getValue(C, i + 1, parameterModes[1]) };
+        return {
+          halted: false,
+          value: getValue(C, i + 1, parameterModes[1]),
+          i: i + 2
+        };
       case 5:
         if (getValue(C, i + 1, parameterModes[1]) !== 0) {
           i = getValue(C, i + 2, parameterModes[2]);
@@ -123,11 +140,9 @@ function computeIntcode(C, phaseSetting, inputFromPrev, phaseGiven = false) {
         i += 4;
         break;
       case 99:
-        console.log("halt");
         return { halted: true };
     }
   }
-  console.log("ping");
   return C;
 }
 
