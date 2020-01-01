5584680b70b5ac2be3fa8167dbaeffdf191a655d
Gurveer Arora <Garora1410@yahoo.co.uk>
2020-01-01 22:05:01 +0000

    debugging attempts

--- out/20_Donut2.js
+++ out/20_Donut2.js
@@ -21,7 +21,7 @@
             this.values.forEach(it => func(it[1], it[0]));
         }
     }
-    const input = '{"SXO":{"ENI":56},"UYO":{"XII":56},"AAO":{"RUO":6,"KHI":50},"RUO":{"AAO":6,"KHI":54},"GTO":{"RFI":72},"VMO":{"XBI":48},"BYO":{"VMI":58},"LGO":{"WDI":52},"ENI":{"SXO":56},"XII":{"UYO":56},"KHI":{"AAO":50,"RUO":54},"RFI":{"GTO":72},"XBI":{"VMO":48},"VMI":{"BYO":58},"WDI":{"LGO":52},"UOI":{"HVO":80},"QAI":{"MBO":58,"ZZO":60},"MBO":{"QAI":58,"ZZO":8},"HVO":{"UOI":80},"ZZO":{"QAI":60,"MBO":8},"YXO":{"GTI":56},"GTI":{"YXO":56},"XIO":{"AOI":78},"AOI":{"XIO":78},"WDO":{"MBI":54},"AOO":{"PII":54},"MBI":{"WDO":54},"PII":{"AOO":54},"UYI":{"ZYO":48,"HVI":4,"OQO":52},"ZYO":{"UYI":48,"HVI":46,"OQO":6},"HVI":{"UYI":4,"ZYO":46,"OQO":50},"RUI":{"PIO":54},"PIO":{"RUI":54},"OQO":{"UYI":52,"ZYO":6,"HVI":50},"OQI":{"WAO":58},"WAO":{"OQI":58},"UOO":{"SXI":74},"SXI":{"UOO":74},"BYI":{"RFO":48},"WAI":{"KHO":52},"RFO":{"BYI":48},"TRI":{"XBO":44},"XBO":{"TRI":44},"KHO":{"WAI":52},"ZYI":{"MWO":54},"MWI":{"TRO":54},"YXI":{"PSO":52},"MDI":{"QAO":44},"PSI":{"MDO":50},"LGI":{"ENO":56},"MWO":{"ZYI":54},"TRO":{"MWI":54},"PSO":{"YXI":52},"QAO":{"MDI":44},"MDO":{"PSI":50},"ENO":{"LGI":56}}';
+    const input = '{"ZZO":{"FDI":2},"FDO":{"REO":4},"REO":{"FDO":4},"FDI":{"ZZO":2},"REI":{"AAO":2},"AAO":{"REI":2}}';
     const stringMatrix = JSON.parse(input);
     function areSamePortal(portal1, portal2) {
         return (portal1.identifier === portal2.identifier &&
@@ -73,10 +73,15 @@
             break;
         }
         const connected = getConnected(chosenPortal);
+        console.log(connected, 444);
         connected.forEach((distance, portal) => {
             const currentDistance = seen.get(portal);
             const newDistance = distance + minDistance;
-            if (currentDistance == null || currentDistance > newDistance) {
+            console.log(visited.get(portal), 333);
+            if (
+            // visited.get(portal) != null && // CAUSES ERROR
+            currentDistance == null ||
+                currentDistance > newDistance) {
                 seen.set(portal, newDistance);
             }
         });
