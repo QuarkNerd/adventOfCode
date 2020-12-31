07d3ec2b939d4d646e3cc1200af3d8cfce794bfa
Gurveer Arora <garora1410@yahoo.co.uk>
2020-12-31 23:00:32 +0000

    Day23 completed

--- 2020/Day23/src/main.rs
+++ 2020/Day23/src/main.rs
@@ -1,56 +1,89 @@
+use std::time::{Instant};
+
+#[derive(Debug)]
+struct Node {
+    value: u32,
+    next: Option<Box<Node>>
+}
+
 fn main() {
     let INPUT = "469217538";
-    let mut cups: Vec<_> = INPUT.chars().map(|x| x.to_string().parse::<u8>().unwrap()).collect();
-
-    for _ in 0..100 {
-        let slice = &[cups.remove(1), cups.remove(1), cups.remove(1)]; // why no temp value dropped error
-        
-        let mut destination_cup = cups[0] - 1;
-        loop {
-            if let Some(pos) = cups.iter().position(|&r| r == destination_cup) {
-                cups.splice(pos+1..pos+1, slice.iter().cloned());
-                break;
-            }
-            if destination_cup == 0 {destination_cup = 9}
-            else {destination_cup -= 1};
+    let len = INPUT.len();
+    
+    let mut next_cup_vec = Vec::new();
+    next_cup_vec.resize((len+1) as usize, 0);
+    let input_vec: Vec<_> = INPUT.chars().map(|x| x.to_string().parse::<u32>().unwrap()).collect();
+    
+    for i in 0..len {
+        if i == len - 1 {
+            next_cup_vec[input_vec[i] as usize] = input_vec[0];
+        } else {
+            next_cup_vec[input_vec[i] as usize] = input_vec[i + 1];
         }
+    }
+    
+    play(&mut next_cup_vec, 100, input_vec[0], 9);
+    
+    let mut final_arrangement = Vec::new();
+    let mut current_cup = 1;
+    loop {
+        current_cup = next_cup_vec[current_cup as usize];
+        if current_cup == 1 {break;}
+        final_arrangement.push(current_cup.to_string());
+    }; 
+    
+    
+    println!("Part one: {}", final_arrangement.join(""));
 
-        cups.rotate_left(1);
+    let now = Instant::now();
+    
+    let mut next_cup_vec = Vec::new();
+    next_cup_vec.resize((1000001) as usize, 0);
+    for i in 0..(len-1) {
+        next_cup_vec[input_vec[i] as usize] = input_vec[i+1];
     };
+    
+    next_cup_vec[input_vec[len-1] as usize] = (len+1) as u32;
+    next_cup_vec[1000000] = input_vec[0];
+    
+    for i in len+1..1000000 {
+        next_cup_vec[i as usize] = (i+1) as u32;
+    }
+    
+    play(&mut next_cup_vec, 10000000, input_vec[0], 1000000);
+    
+    let one_next = next_cup_vec[1];
+    println!("{:?}", now.elapsed());
 
-    let pos_of_one = cups.iter().position(|&r| r == 1).unwrap();
-    cups.rotate_left(pos_of_one);
+    println!("Part two: {}", (one_next as u64) * (next_cup_vec[one_next as usize]) as u64);
+}
 
-    println!("{}", &cups[1..].iter().map(|x| x.to_string()).collect::<Vec<_>>().join(""));
+fn play(next_cup_vec: &mut Vec<u32>, iterations: u32, mut current_cup: u32, max_value: u32) {
+    for _ in 0..iterations {
+        current_cup = step(next_cup_vec, current_cup, max_value); 
+    }  
+}
 
-
-    // let INPUT = "469217538";
-    // let mut cups: Vec<_> = INPUT.chars().map(|x| x.to_string().parse::<u64>().unwrap()).collect();
-
-    // let mut ha = HashSet::new();
-
-    // for i in 10..1000000+1 {
-    //     cups.push(i);
-    // }
-
-    // for a in 0..10000000 {
-    //     if !ha.insert(cups.clone()) {
-    //         println!("{}", a);
-    //     }
-    //     let slice = &[cups.remove(1), cups.remove(1), cups.remove(1)]; // why no temp value dropped error
-        
-    //     let mut destination_cup = cups[0] - 1;
-    //     loop {
-    //         if let Some(pos) = cups.iter().position(|&r| r == destination_cup) {
-    //             cups.splice(pos+1..pos+1, slice.iter().cloned());
-    //             break;
-    //         }
-    //         if destination_cup == 0 {destination_cup = 1000000}
-    //         else {destination_cup -= 1};
-    //     }
-
-    //     cups.rotate_left(1);
-    // };
-
-    // //println!("{:?}", cups)
+fn step(next_cup_vec: &mut Vec<u32>, current_cup: u32, max_value: u32) -> u32 {
+    let move_beginning = next_cup_vec[current_cup as usize];
+    let move_middle = next_cup_vec[move_beginning as usize];
+    let move_end = next_cup_vec[move_middle as usize];
+    
+    let mut destination_cup = current_cup - 1;
+    loop {
+        if [move_beginning, move_middle, move_end].contains(&destination_cup) {
+            destination_cup = destination_cup - 1;
+        } else if destination_cup == 0 {
+            destination_cup = max_value;
+        } else {
+            break;
+        }
+    }
+    
+    let destination_cup_next = next_cup_vec[destination_cup as usize];
+    next_cup_vec[destination_cup as usize] = move_beginning;
+    let new_next = next_cup_vec[move_end as usize];
+    next_cup_vec[move_end as usize] = destination_cup_next;
+    next_cup_vec[current_cup as usize] = new_next;
+    new_next
 }
