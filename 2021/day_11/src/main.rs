2768699f813128d7a3c7ac4ae6f072ba1013c9b9
Gurveer Arora <Garora1410@yahoo.co.uk>
2021-12-11 15:16:33 +0000

    Y2021-D11 complete

--- 2021/day_11/src/main.rs
+++ 2021/day_11/src/main.rs
@@ -1,60 +1,99 @@
-use std::{fmt::Result, collections::HashMap};
+use std::collections::HashMap;
+use std::ops::{Add, AddAssign};
 
 fn main() {
-    let mut input: HashMap<(i8, i8),(u8, bool)> = include_str!("i").lines().enumerate().flat_map(|(i, x)| {
-        x.split("").filter(|x| x != &"").map(str::parse).enumerate().map(|(j, d)| ((i as i8, j as i8),(d.unwrap(), false))).collect::<Vec<_>>()
-    }).collect();
+    let mut input: HashMap<_,_> = include_str!("input").lines().enumerate().flat_map(|(i, row)| 
+        row.split("").filter(|x| !x.is_empty()).map(str::parse).enumerate().map(|(j, d)| ((i as i8, j as i8), Octopus::Charging(d.unwrap()))).collect::<Vec<_>>()
+    ).collect();
 
     let mut flashes = 0;
     let keys = input.keys().map(|x| *x).collect::<Vec<_>>();
-    for _days in 0..1000 {
-        // println!("{} day", _days);
+    for days in 1..=1000 {
         for cell in keys.iter() {
-            input.get_mut(cell).unwrap().0 += 1;
+            input.get_mut(cell).unwrap().increment();
         }
-        let mut fl = true;
-        while fl {
-            fl = false;
-            // println!("fsdf");
+        let mut flash_occurred = true;
+        while flash_occurred {
+            flash_occurred = false;
             for cell in keys.iter() {
-                let z  = input.get_mut(cell).unwrap();
-                if z.0 > 9 && z.1 == false{
-                    z.1 = true;
-                    fl = true;
-                    // println!("{} ---", flashes);
-                flashes += 1;
+                let z  = input.get_mut(cell).unwrap().try_flash();
+                if z {
+                    flash_occurred = true;
+                    flashes += 1;
 
-                for neigh in [
-                    (cell.0 - 1, cell.1), ( cell.0, cell.1), (cell.0 + 1, cell.1),
-                    (cell.0 - 1, cell.1 - 1), ( cell.0, cell.1 - 1), (cell.0 + 1, cell.1 - 1),
-                    (cell.0 - 1, cell.1 + 1), ( cell.0, cell.1 + 1), (cell.0 + 1, cell.1 + 1),
-                    ].into_iter() {
-                        if let Some(n) =  input.get_mut(&neigh) {
-                            if n.1 == false {
-                                n.0 += 1;
+                    [
+                        (cell.0 - 1, cell.1), ( cell.0, cell.1), (cell.0 + 1, cell.1),
+                        (cell.0 - 1, cell.1 - 1), ( cell.0, cell.1 - 1), (cell.0 + 1, cell.1 - 1),
+                        (cell.0 - 1, cell.1 + 1), ( cell.0, cell.1 + 1), (cell.0 + 1, cell.1 + 1),
+                    ].into_iter().for_each(|neigh| {
+                            if let Some(n) =  input.get_mut(&neigh) {
+                                n.increment();
                             }
-                        }
-                    }
-                    //.into_iter().filter_map(|x| input.get_mut(&x));
+                        });
+                }
             }
         }
+        if days == 100 {
+            println!("Part one: {}", flashes);
         }
-        
-let mut count =0;
-        for cell in keys.iter() {
-            let z  = input.get_mut(cell).unwrap();
-            if z.1 {
-                z.1 = false;
-                count +=1;
-                input.get_mut(cell).unwrap().0 = 0;
-            }
+
+        let mut count = 0;
+        for  cell in input.values_mut() {
+            if cell.set_to_charging() {count += 1}
         }
-         if count == 100 {
-            println!("----------{}", _days);
+        if count == 100 {
+            println!("Part two: {}", days);
             break;
-         }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Octopus {
+    Charging(u8),
+    Flashed
+}
+
+impl Octopus {
+    fn increment(&mut self) {
+        *self += 1;
     }
 
-    println!("{:?}", input);
-    println!("{} --", flashes);
+    fn try_flash(&mut self) -> bool {
+        if let Self::Charging(x) = self {
+            if *x > 9 {
+                *self = Self::Flashed;
+                return true;
+            };
+        };
+        false
+    }
+
+    fn set_to_charging(&mut self) -> bool {
+        match self {
+            Self::Charging(_) => return false,
+            Self::Flashed => {
+                *self = Self::Charging(0);
+                return true;
+            },
+        };
+    }
+}
+
+// Basicaly useless 
+impl Add<u8> for Octopus {
+    type Output = Self;
+
+    fn add(self, other: u8) -> Self {
+        match self {
+            Octopus::Charging(x) => Octopus::Charging(x + other),
+            Octopus::Flashed => Octopus::Flashed
+        }
+    }
+}
+
+impl AddAssign<u8> for Octopus {
+    fn add_assign(&mut self, other: u8) {
+        *self = *self + other;
+    }
 }
