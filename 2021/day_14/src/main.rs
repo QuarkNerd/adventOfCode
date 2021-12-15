6abf64df0419764e9c25971a6e341f67c5522daa
Gurveer Arora <garora1410@yahoo.co.uk>
2021-12-15 10:35:31 +0000

    Y2021-D14 cleaned up

--- 2021/day_14/src/main.rs
+++ 2021/day_14/src/main.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
-    // let mut start_str: Vec <_> = "SNVVKOBFKOPBFFFCPBSF".split("").filter(|x| !x.is_empty()).collect();
-    let mut start= "SNVVKOBFKOPBFFFCPBSF";
-    // let mut start: Vec <_> = "NNCB".split("").filter(|x| !x.is_empty()).collect();
-    let steps: HashMap<_, _> = include_str!("include").lines().map(
+    let mut input = include_str!("include").split("\r\n\r\n");
+
+    let start= input.next().unwrap();
+
+    let map: HashMap<_, _> = input.next().unwrap().lines().map(
         |x| {
             let mut spl = x.split(" -> ");
             let mut inp = spl.next().unwrap().chars();
-            let charTuple = (inp.next().unwrap(), inp.next().unwrap());
-            (charTuple, spl.next().unwrap().chars().next().unwrap())
+            let input_chars = (inp.next().unwrap(), inp.next().unwrap());
+            (input_chars, spl.next().unwrap().chars().next().unwrap())
         }
     ).collect();
 
@@ -20,30 +21,38 @@ fn main() {
         acc
     });
 
-    println!("{:?}", steps);
-    println!("{:?}", state);
+    for _ in 0..10 { state = mutate(state, &map) }
+    println!("Part one: {}", calculate_score(&state));
 
-    for i in 0..40 {
-        state = state.into_iter().fold(HashMap::new(), 
-        |mut acc, (chars, count)| {
-            if let Some(&a) = steps.get(&chars) {
+    for _ in 0..30 { state = mutate(state, &map) }
+    println!("Part two: {}", calculate_score(&state));
+}
 
-                let thingOne = (chars.0, a);  
-                let thingTwo = (a, chars.1);  
-                *acc.entry(thingOne).or_insert(0) += count;
-                *acc.entry(thingTwo).or_insert(0) += count;
-            }
-                acc
-        });
-    }
+fn mutate(state: HashMap<(char, char), i64>, map: &HashMap<(char, char), char>) -> HashMap<(char, char), i64> {
+    state.into_iter().fold(HashMap::new(), 
+    |mut acc, (old_chars, count)| {
+        if let Some(&new_char) = map.get(&old_chars) {
+            let pair_one = (old_chars.0, new_char);  
+            let pair_two = (new_char, old_chars.1);  
+            *acc.entry(pair_one).or_insert(0) += count;
+            *acc.entry(pair_two).or_insert(0) += count;
+        } else {
+            panic!("None mutating pairs not implemented");
+        }
+            acc
+    })
+}
 
-    let pool = state.into_iter().fold(HashMap::new(), 
+fn calculate_score(state: &HashMap<(char, char), i64>) -> i64 {
+    let final_counts: Vec<_> = state.iter().fold(HashMap::new(), 
     |mut acc, (chars, count)| {
             *acc.entry(chars.0).or_insert(0) += count;
             *acc.entry(chars.1).or_insert(0) += count;
             acc
-    });
+    }).into_values().collect();
+
+    let lowest = *final_counts.iter().min().unwrap();
+    let highest = final_counts.into_iter().max().unwrap();
     
-    println!("{:?}", pool);
-    println!("{:?}", (8462283437264 as i64 - 1077843463188)/2);
+    (highest + (highest%2) - lowest - (lowest%2))/2
 }
