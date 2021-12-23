d21b7e77bbda6fd00d8214ccbe1f286e78f5cc5a
Gurveer Arora <Garora1410@yahoo.co.uk>
2021-12-23 20:23:42 +0000

    Y2021-D23 complete

--- 2021/day_23/src/main.rs
+++ 2021/day_23/src/main.rs
@@ -1,122 +1,94 @@
-use std::{ops::{Deref, DerefMut}, cmp::{min, max}, collections::HashMap};
+use std::cmp::{min, max};
 
-// use pathfinding::num_traits::Zero;
 extern crate pathfinding;
-
-use pathfinding::*;
 use pathfinding::prelude::dijkstra;
 
 fn main() {
-    let (state, x) = get_starting_node();
-    println!("{:?}", state);
-
+    let state = get_starting_node();
     let neighbours = |n: &Node| {
         let mut neigh = vec![];
-        let mut false_amphipods_per_room = [0;10];
-        let mut shallow_amphipods_per_room = [0;10];
-        let mut hallway_amphipods = [0;11];
-        for amphipod in n.iter() {
+
+        let default_depth = if n.1.is_none() {3} else {5};
+        let mut incorrect_amphipods_in_room = [false; 9];
+        let mut shallowest_amphipod_per_room = [default_depth;10];
+        let mut hallway_amphipods = [false; 11];
+
+        let all_amphipods = get_vec_from_node(n);
+
+        for amphipod in all_amphipods.iter() {
             match amphipod.state {
-               State::Initial(room, deep) => {
-                   false_amphipods_per_room[room] += 1;
-                   if !deep {shallow_amphipods_per_room[room] += 1;}
-               }
-               State::Hallway(pos) => hallway_amphipods[pos] = 1,
-               _ => {}
+               State::Initial(room, depth) => {
+                   incorrect_amphipods_in_room[room] = true;
+                   shallowest_amphipod_per_room[room] = min(shallowest_amphipod_per_room[room], depth);
+                }
+               State::Hallway(pos) => hallway_amphipods[pos] = true,
+               State::Final(depth) => shallowest_amphipod_per_room[amphipod.target_room] = min(shallowest_amphipod_per_room[amphipod.target_room], depth)
             }
         }
 
-        for i in 0..8 {
-            let current = n[i];
+        for (i, current) in all_amphipods.iter().enumerate() {
             match current.state {
-                State::Final => { continue; },
+                State::Final(_) => { continue; },
                 State::Hallway(x) => {
-                    if false_amphipods_per_room[current.target_room] > 0 { continue; }
+                    if incorrect_amphipods_in_room[current.target_room] { continue; }
+                    
                     let range = min(x + 1, current.target_room)..max(current.target_room, x);
-                    if hallway_amphipods[range].into_iter().any(|x| x != &0) { continue; }
-                    let mut new_node = n.clone();
+                    if hallway_amphipods[range].into_iter().any(|x| *x) { continue; }
+                    
+                    let depth = shallowest_amphipod_per_room[current.target_room] - 1;
 
+                    let mut new_node = all_amphipods.clone();
                     new_node[i] = Amphipod {
-                        state: State::Final,
-                        ..current
+                        state: State::Final(depth),
+                        ..*current
                     };
-                    neigh.push((new_node, ((x as i64 - current.target_room as i64).abs() as usize)*current.movement_cost ));
+                    neigh.push((get_node_from_vec(new_node), ((x as i64 - current.target_room as i64).abs() as usize + depth)*current.movement_cost ));
                 },
-                State::Initial(x, deep) => {
+                State::Initial(room, depth) => {
                     // println!("{}", x);
-                    if deep && shallow_amphipods_per_room[x] > 0 {continue;}
+                    if depth != shallowest_amphipod_per_room[room] {continue;}
                     let mut allowed_hallway_postions = vec![];
-                    for pos in x+1..=10 {
+                    for pos in room+1..=10 {
                         if [2, 4, 6, 8].contains(&pos) {continue;}
-                        if hallway_amphipods[pos] != 0 { break; }
+                        if hallway_amphipods[pos] { break; }
                         allowed_hallway_postions.push(pos);
                     }
-                    for pos in (0..x).rev() {
+                    for pos in (0..room).rev() {
                         if [2, 4, 6, 8].contains(&pos) {continue;}
-                        if hallway_amphipods[pos] != 0 { break; }
+                        if hallway_amphipods[pos] { break; }
                         allowed_hallway_postions.push(pos);
                     }
 
                     allowed_hallway_postions.into_iter().for_each(|f| {
-                        let newAmphipod = Amphipod {
+                        let new_amphipod = Amphipod {
                             state: State::Hallway(f),
-                            ..current
+                            ..*current
                         };
-                        let mut new_node = n.clone();
-                        new_node[i] = newAmphipod;
-                        neigh.push((new_node, ((f as i64 - x as i64).abs() as usize)*current.movement_cost));
+                        let mut new_node = all_amphipods.clone();
+                        new_node[i] = new_amphipod;
+                        neigh.push((get_node_from_vec(new_node), ((f as i64 - room as i64).abs() as usize + depth)*current.movement_cost));
                     });
                 }
             }
         }
+
         neigh
     };
 
     let success = |n: &Node| {
-        n.iter().all(|amphipod| {
-            State::Final == amphipod.state
+        let all_amphipods = get_vec_from_node(n);
+        all_amphipods.into_iter().all(|amphipod| {
+            amphipod.state.is_final()
         })
     };
 
-    let result = dijkstra(&state, neighbours, success);
-    let a = result.unwrap();
-    println!("{}", a.1 + x);
-    println!("{}", a.1 + x);
-    println!("{}", a.1 + x);
-    println!("{}", a.1 + x);
+    let result = dijkstra(&state, neighbours, success).unwrap();
+    println!("{}", result.1);
 
-
-    let result = dijkstra(&state, neighbours, |x| *x==get_stage_two());
-    let g = result.unwrap();
-    println!("{:?}", g.1);
-
-    for b in g.0 {
-
-        println!("\n");
-        for c in b.iter() {
-            println!("{:?}", c);
-
-        }
-        // println!("{:?}", b.iter().map(|x| format!("{:?}", x)).join("\n") );
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Node([Amphipod; 8]);
-
-impl Deref for Node {
-    type Target = [Amphipod; 8];
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for Node {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
+struct Node([Amphipod; 8], Option<[Amphipod; 8]>);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Amphipod {
@@ -127,47 +99,44 @@ struct Amphipod {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum State {
-    // Initial inner is room number is they are in A=2 B=4, bboolean determnes if in most depth
-    Initial(usize, bool),
+    // Initial inner is room number is they are in A=2 B=4, followed by depth
+    Initial(usize, usize),
     Hallway(usize),
-    Final,
+    Final(usize) //depth
 }
 
-fn get_starting_node() -> (Node, usize) {
+impl State {
+    fn is_final(&self) -> bool {
+        if let State::Final(_) = &self {
+            return true;
+        };
+        false
+    }
+}
+
+fn get_starting_node() -> Node {
     let mut amphipods = vec![];
-    let rows: Vec<_> = include_str!("input").lines().skip(2).take(2).collect();
-    let mut additional_cost = 6666;
+    let mut rows: Vec<_> = include_str!("input").lines().skip(2).take(2).collect();
+
+    // comment in for part two
+    // rows.insert(1, "  #D#C#B#A#  ");
+    // rows.insert(2, "  #D#B#A#C#  ");
 
     for i in 0..4 {
         let pos = 2*(i + 1);
+        let mut can_be_final = true;
 
-        let amphipod_1 = get_amphipod(&rows[1][(pos+1)..(pos+2)], pos, true, true);
-        let amphipod_2 = get_amphipod(&rows[0][(pos+1)..(pos+2)], pos, amphipod_1.state == State::Final, false);
-        
-        if amphipod_1.state == State::Final {
-            additional_cost -= 2*amphipod_1.movement_cost;
-            if amphipod_2.state == State::Final {
-                additional_cost -= 4*amphipod_1.movement_cost;
-            }
-        }
-
-        amphipods.push(amphipod_1);
-        amphipods.push(amphipod_2);
+        for (depth,row) in rows.iter().enumerate().rev() {
+            let amphipod = get_amphipod(&row[(pos+1)..(pos+2)], pos, can_be_final, depth + 1);
+            can_be_final = amphipod.state.is_final();  
+            amphipods.push(amphipod) 
+        };
     }
 
-    (Node([
-        amphipods[0],
-        amphipods[1],
-        amphipods[2],
-        amphipods[3],
-        amphipods[4],
-        amphipods[5],
-        amphipods[6],
-        amphipods[7]
-    ]), additional_cost)
+    get_node_from_vec(amphipods)
 }
 
-fn get_amphipod(variant: &str, pos: usize, can_be_final: bool, is_in_deep: bool) -> Amphipod {
+fn get_amphipod(variant: &str, pos: usize, can_be_final: bool, depth: usize) -> Amphipod {
     let (target_room, movement_cost) = match variant {
         "A" => (2, 1),
         "B" => (4, 10),
@@ -177,9 +146,9 @@ fn get_amphipod(variant: &str, pos: usize, can_be_final: bool, is_in_deep: bool)
     };
 
     let state = if can_be_final && pos == target_room {
-        State::Final
+        State::Final(depth)
     } else {
-        State::Initial(pos, is_in_deep)
+        State::Initial(pos, depth)
     };
 
     Amphipod {
@@ -189,35 +158,38 @@ fn get_amphipod(variant: &str, pos: usize, can_be_final: bool, is_in_deep: bool)
     }
 }
 
-fn get_stage_two() -> Node {
-    Node([
-        Amphipod {
-            state: State::Final,
-            movement_cost: 1,
-            target_room: 2
-        },
-        Amphipod {
-            state:State::Initial(2, false),
-            movement_cost:10,
-            target_room: 4
-        },
-        Amphipod {
-            state:State::Initial(4, true),
-            movement_cost:1000,
-            target_room: 8
-        },
-        Amphipod {
-            state:State::Final,
-            movement_cost:100,
-            target_room: 6
-        },
-        Amphipod {
-            state:State::Final,
-            movement_cost:100,
-            target_room: 6
-        },
-        Amphipod { movement_cost: 10, target_room: 4, state: State::Hallway(3) },
-        Amphipod { movement_cost: 1, target_room: 2, state: State::Initial(8, true) },
-        Amphipod { movement_cost: 1000, target_room: 8, state: State::Initial(8, false) },
-    ])
+fn get_node_from_vec(v: Vec<Amphipod>) -> Node {
+    let mut n = Node([
+        v[0],
+        v[1],
+        v[2],
+        v[3],
+        v[4],
+        v[5],
+        v[6],
+        v[7]
+    ], None);
+
+    if v.len() == 16 {
+        n.1 = Some([
+            v[8],
+            v[9],
+            v[10],
+            v[11],
+            v[12],
+            v[13],
+            v[14],
+            v[15]
+        ]);
+    };
+
+    n
+}
+
+fn get_vec_from_node(n: &Node) -> Vec<Amphipod> {
+    let mut all_amphipods: Vec<_> = n.0.iter().map(|x| *x).collect();
+    if let Some(rest) = n.1 {
+        rest.iter().for_each(|x| all_amphipods.push(*x));
+    }
+    all_amphipods
 }
