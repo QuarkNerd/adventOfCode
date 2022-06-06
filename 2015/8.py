c2a6ae8b3c0f49a6578cc37e6400ed59ba834800
Gurveer Arora <garora1410@yahoo.co.uk>
2022-06-06 13:03:02 +0100

    2015 day 8 complete

--- 2015/8.py
+++ 2015/8.py
@@ -1,13 +1,7 @@
+from itertools import count
+
 input = open("input/8", "r").read().strip()
 lines = input.splitlines()
 
-
 print(sum(map(lambda str: len(str) - len(eval(str)), lines)))
-print(sum(map(lambda str: len(repr(str)) - len(str), lines)))
-
-lo = lambda str: len(repr(str)) - len(str)
-
-print(lo(lines[0]))
-print(lo(lines[1]))
-print(lo(lines[2]))
-print(lo(lines[3]))
+print(sum(map(lambda str: len(repr(str)) + str.count('"') - len(str), lines)))
