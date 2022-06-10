6690991f5e97e4094b480a8898aad3bea10a576f
Gurveer Arora <garora1410@yahoo.co.uk>
2022-06-10 10:52:17 +0100

    2015 update day 9 to use utility function

--- 2015/9.py
+++ 2015/9.py
@@ -1,36 +1,12 @@
-from itertools import permutations
-import re
-
-def sliding_window(elements, window_size):
-    if len(elements) <= window_size:
-        return frozenset()
-    for i in range(len(elements) - window_size + 1):
-        yield frozenset(elements[i:i+window_size])
+import visitAllNodes
 
 input = open("input/9", "r").read().strip()
 lines = input.splitlines()
 
 distances = {}
-all_locations = set("")
 
 for journey in lines:
     route, distance = journey.split(" = ")
     distances[frozenset(route.split(" to "))] = int(distance)
-    all_locations.update(route.split(" to "))
 
-
-minDistance = 10**10
-maxDistance = 0
-
-for route in permutations(all_locations):
-    pairs = sliding_window(route, 2)
-    dist = sum(map(lambda p: distances[p], pairs))
-
-    if dist < minDistance:
-        minDistance = dist
-
-    if dist > maxDistance:
-        maxDistance = dist
-
-print(minDistance)
-print(maxDistance)
+print(visitAllNodes.getWeight(distances))
