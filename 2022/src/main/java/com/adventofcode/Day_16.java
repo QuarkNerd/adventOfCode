0b5c6ddeb50257df363fe68ade25c893dddaa642
Gurveer Arora <garora1410@yahoo.co.uk>
2022-12-16 20:58:38 +0000

    Y2022-D16 completed

--- 2022/src/main/java/com/adventofcode/Day_16.java
+++ 2022/src/main/java/com/adventofcode/Day_16.java
@@ -11,26 +11,101 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Day_16 extends SolverBase {
-    public static void main(String[] args) { (new Day_16()).run(); }
+
+    static public HashMap<String, Integer> pressures = new HashMap<>();
+    static public DualKeyMap<String, Integer> directDuration = new DualKeyMap<>();
+    static public HashMap<String, List<String>> connection = new HashMap<>();
+
+    public static void main(String[] args) {
+        (new Day_16()).run();
+    }
 
     public SolutionPair solve(String input) {
-        HashMap<String, Integer> pressures = new HashMap<>();
-        HashMap<String, List<String>> connection = new HashMap<>();
+        parseInputToGenerateConnectionsAndPressures(input);
+        generateAndSaveDirectConnectionMap();
 
+        List<Route> activeRoutes = new ArrayList<>();
+        activeRoutes.add(Route.fromNode("AA"));
+
+        Set<String> allNodes = pressures.keySet();
+        int maxPressurePartOne = 0;
+        Map<Set<String>, Integer> finalPossiblePressuresPartTwo = new HashMap();
+
+        while (activeRoutes.size() > 0) {
+            List<Route> nextActiveRoutes = new ArrayList<>();
+            for (Route route : activeRoutes) {
+
+                List<Route> toAdd = new ArrayList<>();
+                for (String node : allNodes) {
+                    if (!route.canVisit(node, 30)) continue;
+                    toAdd.add(route.add(node));
+                }
+                nextActiveRoutes.addAll(toAdd);
+
+                if (toAdd.isEmpty()) {
+                    int routePressure = route.calculatePressureAt(30);
+                    if (routePressure > maxPressurePartOne) {
+                        maxPressurePartOne = routePressure;
+                    }
+                }
+
+                long nextRoutesPartTwo = toAdd.stream().filter(x -> x.timeTaken <= 26).count();
+                if (nextRoutesPartTwo == 0) {
+                    Set<String> set = route.getAllNodes();
+                    Integer currentValue = finalPossiblePressuresPartTwo.getOrDefault(set, null);
+                    int routePressure = route.calculatePressureAt(26);
+                    if (currentValue == null || currentValue < routePressure) {
+                        finalPossiblePressuresPartTwo.put(set, routePressure);
+                    }
+                }
+
+            }
+            activeRoutes = nextActiveRoutes;
+        }
+
+        int partTwoMaxPressure = 0;
+        List<Set<String>> sortedRoutes = finalPossiblePressuresPartTwo
+                .entrySet()
+                .stream()
+                .sorted((x,y) -> x.getValue() > y.getValue() ? -1: 1)
+                .map(x -> x.getKey())
+                .collect(Collectors.toList());
+        while (!sortedRoutes.isEmpty()) {
+            Set<String> routeA = sortedRoutes.remove(0);
+            int routePressureA = finalPossiblePressuresPartTwo.get(routeA);
+
+            if (routePressureA < partTwoMaxPressure) break;
+
+            for (Set<String> routeB: sortedRoutes) {
+                boolean overlap = routeB.stream().anyMatch(x -> routeA.contains(x) && !x.equals("AA"));
+                int total = routePressureA + finalPossiblePressuresPartTwo.get(routeB);
+
+                if (!overlap) {
+                    if (total > partTwoMaxPressure) {
+                        partTwoMaxPressure = total;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return new SolutionPair(maxPressurePartOne, partTwoMaxPressure);
+    }
+
+    private void parseInputToGenerateConnectionsAndPressures(String input) {
         Pattern numbersPattern = Pattern.compile("-?\\d+");
         Pattern tunnelPattern = Pattern.compile("[A-Z][A-Z]+");
 
-        for (String tunnelString: input.split(System.lineSeparator())) {
+        for (String tunnelString : input.split(System.lineSeparator())) {
             Matcher tunnelMatcher = tunnelPattern.matcher(tunnelString);
             tunnelMatcher.find();
+
             String tunnel = tunnelMatcher.group();
 
             List<String> connections = new ArrayList<>();
-
             while (tunnelMatcher.find()) {
                 connections.add(tunnelMatcher.group());
             }
-
             connection.put(tunnel, connections);
 
             Matcher numberMatcher = numbersPattern.matcher(tunnelString);
@@ -40,35 +115,34 @@ public class Day_16 extends SolverBase {
             if (num > 0 || tunnel.equals("AA")) {
                 pressures.put(tunnel, num);
             }
-
         }
+    }
 
-        List<String> bb = pressures.keySet().stream().collect(Collectors.toList());
+    private void generateAndSaveDirectConnectionMap() {
+        List<String> cavesToMap = pressures.keySet().stream().collect(Collectors.toList());
 
-        DualKeyMap<String, Integer> directDist = new DualKeyMap<>();
-
-        while (!bb.isEmpty()) {
-            int connectedCount =0;
-            String start = bb.remove(0);
+        while (!cavesToMap.isEmpty()) {
+            int connectedCount = 0;
+            String start = cavesToMap.remove(0);
             HashSet<String> current = new HashSet<>();
             current.add(start);
             HashSet<String> visited = new HashSet<>();
             visited.add(start);
             int steps = 0;
 
-            while (bb.size() != connectedCount) {
+            while (cavesToMap.size() != connectedCount) {
                 steps++;
 
                 HashSet<String> newCurrent = new HashSet<>();
-                for (String cur: current) {
-                    for(String connected: connection.get(cur)) {
+                for (String cur : current) {
+                    for (String connected : connection.get(cur)) {
                         if (visited.contains(connected)) continue;
                         newCurrent.add(connected);
                         visited.add(connected);
 
-                        if(bb.contains(connected)) {
+                        if (cavesToMap.contains(connected)) {
                             connectedCount++;
-                            directDist.put(start, connected, steps);
+                            directDuration.put(start, connected, steps);
                         }
                     }
                     current = newCurrent;
@@ -76,124 +150,57 @@ public class Day_16 extends SolverBase {
 
             }
         }
-
-        List<Route> current = new ArrayList<>();
-        List<String> path = new ArrayList<>();
-        path.add("AA");
-        current.add(new Route(path, 0, 0));
-
-        Set<String> all = pressures.keySet();
-        Set<Route> finall = new HashSet<>();
-
-        while (current.size() > 0) {
-            List<Route> newCurrent = new ArrayList<>();
-            for(Route b: current) {
-                String latest = b.path.get(b.path.size() - 1);
-                List<Route> toAdd = new ArrayList<>();
-
-                for (String node: all) {
-                    if (b.path.contains(node)) continue;
-                    int dist = b.timeTaken +1 + directDist.get(latest, node);
-                    int distRemaining = 30-dist;
-
-                    if (distRemaining <= 0) continue;
-
-                    List<String> cloned = new ArrayList(b.path);
-                    cloned.add(node);
-
-                    int pressurePlus = (30 - dist)*pressures.get(node);
-                    toAdd.add(new Route(cloned, dist, b.pressure + pressurePlus));
-                }
-
-
-                newCurrent.addAll(toAdd);
-                if (toAdd.isEmpty()) {
-                    finall.add(b);
-                }
-
-
-            }
-            current = newCurrent;
-        }
-
-
-//        int ggg = finall.stream().mapToInt(x -> x.pressure).max().orElseThrow(NoSuchElementException::new);;
-
-        List<Route> o= finall.stream().sorted((a, b) -> a.pressure > b.pressure ? -1 : 1).collect(Collectors.toList());
-
-//        int max = 0;
-//        for (int i = 0; i < o.size(); i++) {
-//           for (int j = i; j < o.size(); j++) {
-//               int finalJ = j;
-//               B mmmmmmm = o.get(finalJ);
-//               long overlapC = o.get(i).path.stream().filter(
-//                       x -> mmmmmmm.path.contains(x)
-//               ).count();
-//
-//               if (overlapC == 1) {
-//                   int pres = o.get(i).pressure + mmmmmmm.pressure;
-//                   if (pres > max) {
-//                       max = pres;
-//                   }
-//               }
-//           }
-//        }
-
-        return new SolutionPair(o.get(0).pressure, 0);
     }
-
-    class Route {
-        public final List<String> path;
-        public int timeTaken;
-        public int pressure;
-
-
-        Route(List<String> path, int timeTaken, int pressure) {
-            this.path = path;
-            this.timeTaken = timeTaken;
-            this.pressure = pressure;
-        }
-
-        public void print(){
-            System.out.println(path);
-            System.out.println(pressure);
-        }
-    }
-
-    class DualKeyMap<K, V> {
-        private Map<K, Map<K, V>> inner;
-
-        DualKeyMap() {
-            inner = new HashMap<>();
-        }
-
-        public void put(K keyOne, K keyTwo, V value) {
-
-            inner.putIfAbsent(keyOne, new HashMap<>());
-            inner.get(keyOne).put(keyTwo, value);
-            inner.putIfAbsent(keyTwo, new HashMap<>());
-            inner.get(keyTwo).put(keyOne, value);
-        }
-
-        public V get(K keyOne, K keyTwo) {
-            return inner.get(keyOne).get(keyTwo);
-        }
-    }
-
 }
 
-//                        .flatMap(x -> {
-//                             int dist = directDist.get(latest, x);
-//                             int distRemaining = 30-(b.dist + 1 + dist);
-//                             if (distRemaining <= 0) {
-//                                 return Stream.empty();
-//                             }
-//
-//                            List<String> cloned = new ArrayList(b.path);
-//                            cloned.add(x);
-//
-//                            int pressurePlus = (30 - dist)*pressures.get(x);
-////                            newCurrent.add(new B(cloned, dist, b.pressure + pressurePlus));
-//
-//                             return Stream.of(new B(cloned, dist, b.pressure + pressurePlus));
-//                        })
+class Route {
+    static public Route fromNode(String node) {
+        List<String> path = new ArrayList<>();
+        path.add(node);
+        return new Route(path, 0);
+    }
+
+    public final int timeTaken;
+
+    public String getCurrentNode() {
+        return this.path.get(this.path.size() - 1);
+    }
+
+    public Route add(String node) {
+        List<String> clonedPath = new ArrayList(this.path);
+        clonedPath.add(node);
+
+        int newTime = this.timeTaken + Day_16.directDuration.get(node, this.getCurrentNode()) + 1;
+        return new Route(clonedPath, newTime);
+    }
+
+    public Set<String> getAllNodes() {
+        return this.path.stream().collect(Collectors.toSet());
+    }
+
+    public boolean canVisit(String node, int time) {
+        if (this.path.contains(node)) return false;
+        int totalTimeTaken = this.timeTaken + Day_16.directDuration.get(this.getCurrentNode(), node) + 1;
+        int timeRemaining = time - totalTimeTaken;
+        return timeRemaining > 0;
+    }
+
+    public int calculatePressureAt(int time) {
+        int totalPressure = 0;
+        int currentTime = 0;
+
+        for (int i = 0; i < this.path.size() - 1; i++) {
+            int pressure = Day_16.pressures.get(this.path.get(i + 1));
+            currentTime += Day_16.directDuration.get(this.path.get(i + 1), this.path.get(i)) + 1;
+            totalPressure += (time - currentTime) * pressure;
+        }
+        return totalPressure;
+    }
+
+    private final List<String> path;
+
+    private Route(List<String> path, int timeTaken) {
+        this.path = path;
+        this.timeTaken = timeTaken;
+    }
+}
