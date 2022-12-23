7f80c8bcd438700327dc23411c3cacd13f7cc039
Gurveer Arora <garora1410@yahoo.co.uk>
2022-12-23 21:27:51 +0000

    Y2022-D15 completed faster

--- 2022/src/main/java/com/adventofcode/Node.java
+++ 2022/src/main/java/com/adventofcode/Node.java
@@ -3,16 +3,24 @@ package com.adventofcode;
 class Node {
     int x;
     int y;
+    int z;
 
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Node(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + z;
         result = prime * result + y;
         result = prime * result + x;
         return result;
@@ -27,6 +35,8 @@ class Node {
         if (getClass() != obj.getClass())
             return false;
         Node other = (Node) obj;
+        if (z != other.z)
+            return false;
         if (y != other.y)
             return false;
         if (x != other.x)
