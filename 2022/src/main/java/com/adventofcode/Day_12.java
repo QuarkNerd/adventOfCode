bdba3cdc6484213aebd2b7a15c412ce39b25ab26
Gurveer Arora <garora1410@yahoo.co.uk>
2022-12-25 21:28:13 +0000

    Y2022-D12 complete

--- 2022/src/main/java/com/adventofcode/Day_12.java
+++ 2022/src/main/java/com/adventofcode/Day_12.java
@@ -1,164 +1,79 @@
 package com.adventofcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.ToLongFunction;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
+import java.util.stream.Stream;
 
-public class Day_11 extends SolverBase {
-    public static void main(String[] args) { (new Day_11()).run(); }
+public class Day_12 extends SolverBase {
+    public static void main(String[] args) { (new Day_12()).run(); }
+
+    private static Map<Node, Character> map2 = new HashMap<>();
 
     public SolutionPair solve(String input) {
-        EvilLuggageTroop troopPartOne = EvilLuggageTroop.parse(input, false);
-        for(int i = 0; i < 20; i++) {
-            troopPartOne.processLuggage();
+        String[] map = input.lines().toArray(String[]::new);
+
+        Node start = null;
+        Node end = null;
+        for (int i = 0; i < map.length; i++) {
+            String line = map[i];
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                Node n = new Node(j, i);
+                map2.put(n, c);
+                if (c == 'S') start = n;
+                if (c == 'E') end = n;
+            }
         }
-        EvilLuggageTroop troopPartTwo = EvilLuggageTroop.parse(input, true);
-        for(int i = 0; i < 10000; i++) {
-            troopPartTwo.processLuggage();
-        }
-        return new SolutionPair(troopPartOne.getMonkeyBusinessLevel(), troopPartTwo.getMonkeyBusinessLevel());
+
+        long sol = ShortestPathBFS.find(
+                start,
+                x -> map2.get(x) == 'E',
+                this::getNextStatesPartOne
+                ,true
+        ).dist;
+
+        // Done in reverse
+        Node finalStart = start;
+        long sol2 = ShortestPathBFS.find(
+                end,
+                x -> map2.get(x) == 'a' || x == finalStart,
+                this::getNextStatesPartTwo
+                ,true
+        ).dist;
+
+        return new SolutionPair(sol, sol2);
     }
 
-    static class EvilLuggageTroop {
-        private final LuggageMonkey[] monkeys;
-        private final long moduloStress;
-        private final boolean isPartTwo;
-
-        public static EvilLuggageTroop parse(String definition, boolean isPartTwo) {
-            LuggageMonkey[] monkeys = Arrays.stream(definition.split(System.lineSeparator() + System.lineSeparator()))
-                    .map(LuggageMonkey::parse)
-                    .toArray(LuggageMonkey[]::new);
-            EvilLuggageTroop troop = new EvilLuggageTroop(monkeys, isPartTwo);
-            for(LuggageMonkey monkey: monkeys) {
-                monkey.setTroop(troop);
-            }
-            return troop;
-        }
-
-        public void throwLuggage(long worryLevel, int monkeyNum) {
-            monkeys[monkeyNum].catchLuggage(worryLevel);
-        }
-
-        public void processLuggage() {
-            for(LuggageMonkey monkey: monkeys) {
-                monkey.processLuggage();
-            }
-        }
-
-        public long getMonkeyBusinessLevel() {
-            Integer[] counts = Arrays.stream(this.monkeys).map(LuggageMonkey::getProcessCount).toArray(Integer[]::new);
-            Arrays.sort(counts, Collections.reverseOrder());
-            return (long)counts[0] * (long)counts[1];
-        }
-
-        public long reduceStress(long stress) {
-            return isPartTwo ? stress % moduloStress : stress / 3;
-        }
-
-        private EvilLuggageTroop(LuggageMonkey[] monkeys, boolean isPartTwo) {
-            this.monkeys = monkeys;
-            this.isPartTwo = isPartTwo;
-            this.moduloStress = Arrays.stream(monkeys).map(LuggageMonkey::getDivisor).reduce(1,(x,y)->x*y);
-        }
+    private Stream<Node> getNextStatesPartOne(Node n, long _dist) {
+        char current = map2.get(n);
+        return getConnected(n).stream().filter(connected -> {
+            Character next = map2.get(connected);
+            return next != null && (next - current <= 1 || next == 'E' || current == 'S');
+        });
     }
 
-    static class LuggageMonkey {
-        private final List<Long> items;
-        private final ToLongFunction<Long> operation;
-        private final int divisor;
-        private final int targetIfTrue;
-        private final int targetIfFalse;
-        private int processCount = 0;
-
-        private EvilLuggageTroop troop;
-
-        public void setTroop(EvilLuggageTroop troop) {
-            this.troop = troop;
-        }
-
-        public int getProcessCount() {
-            return processCount;
-        }
-
-        public int getDivisor() {
-            return divisor;
-        }
-
-        public static LuggageMonkey parse(String definition) {
-            String[] spl = definition.split(System.lineSeparator());
-
-            List<Long> items = new ArrayList<>();
-            Matcher itemsMatcher = numbersPattern.matcher(spl[1]);
-            while (itemsMatcher.find()) {
-                items.add(Long.valueOf(itemsMatcher.group()));
-            }
-
-            ToLongFunction<Long> op = parseOperation(spl[2]);
-
-            Matcher divisorMatcher = numbersPattern.matcher(spl[3]);
-            divisorMatcher.find();
-            int divisor = Integer.parseInt(divisorMatcher.group());
-
-            Matcher targetIfTrueMatcher = numbersPattern.matcher(spl[4]);
-            targetIfTrueMatcher.find();
-            int targetIfTrue = Integer.parseInt(targetIfTrueMatcher.group());
-
-            Matcher targetIfFalseMatcher = numbersPattern.matcher(spl[5]);
-            targetIfFalseMatcher.find();
-            int targetIfFalse = Integer.parseInt(targetIfFalseMatcher.group());
-            return new LuggageMonkey(items, op, divisor, targetIfTrue, targetIfFalse);
-        }
-
-        public void catchLuggage(long item) {
-            this.items.add(item);
-        }
-
-        public void processLuggage() {
-            while (this.items.size() > 0) {
-                this.processCount++;
-
-                Long item = this.items.remove(0);
-                item = this.operation.applyAsLong(item);
-                item = this.troop.reduceStress(item);
-
-                if (item % this.divisor == 0) {
-                    this.troop.throwLuggage(item, targetIfTrue);
-                } else {
-                    this.troop.throwLuggage(item, targetIfFalse);
-                }
-            }
-        }
-
-        private static final Pattern numbersOrOldPattern = Pattern.compile("(old|\\d+)");
-        private static final Pattern operatorPattern = Pattern.compile("[*+]");
-        private static final Pattern numbersPattern = Pattern.compile("\\d+");
-        private static ToLongFunction<Long> parseOperation(String opLine) {
-            Matcher inputMatcher = numbersOrOldPattern.matcher(opLine);
-            inputMatcher.find();
-            Integer first = Util.safeParseInteger(inputMatcher.group());
-            inputMatcher.find();
-            Integer second = Util.safeParseInteger(inputMatcher.group());
-
-            Matcher opMatcher = operatorPattern.matcher(opLine);
-            opMatcher.find();
-
-            if (opMatcher.group().equals("+")) {
-                return (x) -> (first == null ? x : first) + (second == null ? x : second);
-            }
-            return (x) -> (first == null ? x : first) * (second == null ? x : second);
-        }
-
-        private LuggageMonkey(List<Long> items, ToLongFunction<Long> operation, int divisor, int targetIfTrue, int targetIfFalse) {
-            this.items = items;
-            this.operation = operation;
-            this.divisor = divisor;
-            this.targetIfTrue = targetIfTrue;
-            this.targetIfFalse = targetIfFalse;
-        }
+    private Stream<Node> getNextStatesPartTwo(Node n, long _dist) {
+        char current = map2.get(n);
+        return getConnected(n).stream().filter(connected -> {
+            Character next = map2.get(connected);
+            return next != null && (current - next <= 1 || next == 'S' || current == 'E');
+        });
     }
 
+
+    private List<Node> getConnected(Node cube) {
+        List<Node> connected = new ArrayList<>();
+        int[] pp = new int[]{-1, 1};
+        for (int dx : pp) {
+            int x = cube.x + dx;
+            connected.add(new Node(x, cube.y, cube.z));
+        }
+        for (int dy : pp) {
+            int y = cube.y + dy;
+            connected.add(new Node(cube.x, y, cube.z));
+        }
+        return connected;
+    }
 }
---
+++ 2022/src/main/java/com/adventofcode/ShortestPathBFS.java
@@ -0,0 +1,2 @@
+package com.adventofcode;public class ShortestPathBFS {
+}
