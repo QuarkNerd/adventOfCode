2b626882ebcde9023422fbd8b7abe9bfc0ec4810
Gurveer Arora <garora1410@yahoo.co.uk>
2024-08-17 18:00:51 +0100

    Y2023-Y17

--- 2023/Day17-scala/build.sbt
+++ 2023/Day17-scala/build.sbt
@@ -1,12 +1,8 @@
-val scala3Version = "3.3.1"
+ThisBuild / version := "0.1.0-SNAPSHOT"
 
-lazy val root = project
-  .in(file("."))
+ThisBuild / scalaVersion := "3.3.3"
+
+lazy val root = (project in file("."))
   .settings(
-    name := "Scala 3 Project Template",
-    version := "0.1.0-SNAPSHOT",
-
-    scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    name := "Day17-Scala"
   )
--- 2023/Day17-scala/src/main/scala/Main.scala
+++ 2023/Day17-scala/src/main/scala/Main.scala
@@ -1,111 +1,108 @@
-import scala.compiletime.ops.boolean 
+import scala.collection.mutable
 import scala.collection.mutable.Set
 import scala.collection.mutable.Map
+import scala.io.Source
+
+// Fairly slow on part 2, could maybe be sped up by pruning nodes based on recentCount
+// i.e, node x,y where you have been going for 8 blocks, is worse than going for 4 blocks
+
+//// Part One
+//val MAXIMUM_DISTANCE = 3;
+//val MINIMUM_DISTANCE = 1;
+
+// Part Two
+val MAXIMUM_DISTANCE = 10;
+val MINIMUM_DISTANCE = 4;
 
 enum Direction:
-  def opposite = 
+  def opposite: Direction =
     this match
       case Up => Direction.Down
       case Down => Direction.Up
       case Right => Direction.Left
-      case Left => Direction.Right 
+      case Left => Direction.Right
   case Up, Right, Down, Left
 
 class Node(val x: Int, val y: Int, val recentDir: Direction, val recentCount: Int) {
-    def getconnected(height: Int, width: Int): Array[Node] =
-      Direction.values
+  def getConnected(height: Int, width: Int): Array[Node] =
+    var dirs = Direction.values
       .filter(dir =>
-        dir != recentDir.opposite && 
-        !(recentCount == 10 && dir == this.recentDir)
-        )
+        (dir != recentDir.opposite || recentCount == 0)
+          && !(recentCount == MAXIMUM_DISTANCE && dir == this.recentDir)
+      )
+
+    if (recentCount == 0) {
+      dirs = Direction.values
+    } else if (recentCount < MINIMUM_DISTANCE) {
+      dirs = Array(recentDir)
+    }
+
+
+    dirs
       .map(dir => {
-        val newRecentCount = if (dir == this.recentDir) this.recentCount + 1 else 4
-        val shift = if (dir == this.recentDir) 1 else 4
+        val newRecentCount = if (dir == this.recentDir) this.recentCount + 1 else 1
         dir match
-          case Direction.Up => new Node(this.x, this.y + shift, dir, newRecentCount)
-          case Direction.Down => new Node(this.x, this.y - shift, dir, newRecentCount)
-          case Direction.Right => new Node(this.x + shift, this.y, dir, newRecentCount)
-          case Direction.Left => new Node(this.x - shift, this.y, dir, newRecentCount)
-          // this wont count the jumped areas
+          case Direction.Up => new Node(this.x, this.y + 1, dir, newRecentCount)
+          case Direction.Down => new Node(this.x, this.y - 1, dir, newRecentCount)
+          case Direction.Right => new Node(this.x + 1, this.y, dir, newRecentCount)
+          case Direction.Left => new Node(this.x - 1, this.y, dir, newRecentCount)
       }).filter(node => {
         0 <= node.x && node.x < width && 0 <= node.y && node.y < height
       })
-    
-    override def equals(that: Any): Boolean =
-      that match
-    {
-        case that: Node => x == that.x && y == that.y && 
-                  recentCount == that.recentCount && recentDir == that.recentDir
-        case _ => false
-    }
-    override def hashCode: Int = {
-        val prime = 31
-        var result = 1
-        result = prime * result + x;
-        result = prime * result + y;
-        result = prime * result + recentCount;
-        result = prime * result + recentDir.ordinal;
 
-        return result
+  override def equals(that: Any): Boolean =
+    that match
+    {
+      case that: Node => x == that.x && y == that.y &&
+        recentCount == that.recentCount && recentDir == that.recentDir
+      case _ => false
     }
+  override def hashCode: Int = {
+    val prime = 31
+    var result = 1
+    result = prime * result + x;
+    result = prime * result + y;
+    result = prime * result + recentCount;
+    result = prime * result + recentDir.ordinal;
+
+    return result
+  }
 }
 
-@main def solve: Unit =
-  val worldMap = scala.io.Source.fromFile("input").mkString.split("\r\n").map(line => line.toArray.map(x => x.toInt - '0'))
-  println("worldMap")
-  println(worldMap(0)(0))
-  // val uu = Array(3)
-  // val ii = Array(3)
-  // uu.copyToArray(ii, 0, 4)
-  // println(ii(1))
-
+@main def run() =
+  val worldMap = Source.fromResource("inp").mkString.split("\r\n").map(line => line.toArray.map(x => x.toInt - '0'))
   val getConnectedNodes = (startNode: Node) => {
-    startNode.getconnected(worldMap.length, worldMap(0).length)
+    startNode.getConnected(worldMap.length, worldMap(0).length)
       .map(node => (worldMap(node.y)(node.x), node))
   }
 
-  val isEnd = (node: Node) => node.x == worldMap(0).length - 1 && node.y == worldMap.length - 1
+  val isEnd = (node: Node) => node.x == worldMap(0).length - 1 && node.y == worldMap.length - 1 && node.recentCount >= MINIMUM_DISTANCE
 
-  val start = new Node(0, 0, Direction.Down, 0)
+  val start = new Node(0, 0, Direction.Left, 0)
   println(getShortestPath(start, getConnectedNodes, isEnd))
 
 
-def getShortestPath[A](startNode: A, getConnectedNodes: A => Array[(Int, A)], isEnd: A => Boolean): Int =
-  val workingNodes = Set(startNode)
-  // val done: Set[A] = Set()
-  val distMap = Map(startNode -> 0)
+def getShortestPath(startNode: Node, getConnectedNodes: Node => Array[(Int, Node)], isEnd: Node => Boolean): Int =
+  val workingNodes = mutable.Set(startNode)
+  val distMap = mutable.Map(startNode -> 0)
   var currentDist = 0
   var current = startNode;
 
-  var i = 0
-  // before: Long = 1694035209622
-  // totalTime: Long = 803
   while(!isEnd(current)) {
-    i += 1
-    // println(i)
-    val before = System.currentTimeMillis(); 
+    val before = System.currentTimeMillis();
     current = workingNodes.minBy(distMap)
-    println("min")
-    println(System.currentTimeMillis-before)
-    // println(current.asInstanceOf[Node].lastThree.mkString)
-    // println("current")
-    // // println(current.asInstanceOf[Node].y)
-    // // println(distMap.get(current).get)
     workingNodes -= current
-    // done += current && !done.contains(node)
 
-    currentDist = distMap.get(current).get
+//    if /
+
+    currentDist = distMap(current)
     for ((dist, node) <- getConnectedNodes(current)) {
-      // println("dist")
-      // println(dist)
       val totalDistance = currentDist + dist
       if (totalDistance < distMap.getOrElse(node, Int.MaxValue)) {
         workingNodes += node
         distMap += (node -> totalDistance)
       }
     }
-    println("all")
-    println(System.currentTimeMillis-before)
   }
   currentDist
 
