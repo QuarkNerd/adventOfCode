01f4314534ced8c402fb78d8e941e60a901b1d9c
Gurveer Arora <garora1410@yahoo.co.uk>
2024-12-03 06:58:24 +0000

    Y2024 initial Day03

--- 2024/Day03/index.js
+++ 2024/Day03/index.js
@@ -1,7 +1,26 @@
 function solve(inputString) {
-  const input = inputString.split(/\r?\n/).map((x) => x.split("   "));
+  const regex = /(\w+'?t?)\(((\d{1,3}),(\d{1,3}))?\)/g;
+  const matches = inputString.matchAll(regex);
 
-  return [partOneTotal, partTwoTotal];
+  let g = true;
+
+  const one = matches
+    .map((x) => {
+      console.log(x[0]);
+      if (x[0] === "do()") {
+        g = true;
+      }
+      if (x[0] == "don't()") {
+        g = false;
+      }
+      if (x[1] == "mul" && g) {
+        console.log(x[3], "www", x[4]);
+        return parseInt(x[3]) * parseInt(x[4]);
+      }
+      return 0;
+    })
+    .reduce((a, b) => a + b, 0);
+  return [one, 0];
 }
 
 module.exports = solve;
