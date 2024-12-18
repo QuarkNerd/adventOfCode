1265179b36dad1c970d214349fce3fb761e7b1c4
Gurveer Arora <garora1410@yahoo.co.uk>
2024-12-18 18:04:46 +0000

    Y2024 Day Day15 complete

--- 2024/Day15/index.js
+++ 2024/Day15/index.js
@@ -3,46 +3,108 @@ const {
   createCharToCoor,
   applyDirection,
   parseDirection,
+  nodeToString,
+  directionCoors,
 } = require("../../shared/js/2dcoor");
 
 function solve(inputString) {
-  const [mapString, directions] = inputString.split(/\r?\n\r?\n/);
-
-  const map = to2dArray(mapString);
-  let charToCoor = createCharToCoor(map);
-  let bot = charToCoor["@"][0];
-  map[bot.y][bot.x] = ".";
-
-  directions
+  const [mapString, directionsString] = inputString.split(/\r?\n\r?\n/);
+  const directions = directionsString
     .split("")
     .filter((x) => x !== "\r" && x !== "\n")
-    .forEach((dirStr) => {
-      const direction = parseDirection(dirStr);
-      let positionOfInterest = applyDirection(bot, direction);
-      let target = map[positionOfInterest.y]?.[positionOfInterest.x];
+    .map((x) => parseDirection(x));
 
-      if (target === "#") return;
-      if (target === ".") {
-        bot = positionOfInterest;
-        return;
-      }
-      while (target === "O") {
-        positionOfInterest = applyDirection(positionOfInterest, direction);
-        target = map[positionOfInterest.y]?.[positionOfInterest.x];
-      }
-      if (target === "#") return;
-      bot = applyDirection(bot, direction);
-      map[bot.y][bot.x] = ".";
-      map[positionOfInterest.y][positionOfInterest.x] = "O";
-    });
+  return [
+    solvePart(mapString, directions, false),
+    solvePart(mapString, directions, true),
+  ];
+}
+
+function solvePart(mapString, directions, isPartTwo = false) {
+  const map = getMap(mapString, isPartTwo);
 
   charToCoor = createCharToCoor(map);
+  bot = charToCoor["@"][0];
+  map[bot.y][bot.x] = ".";
 
-  const partOne = charToCoor["O"]
+  directions.forEach((direction) => {
+    const nextBot = applyDirection(bot, direction);
+    const target = map[nextBot.y][nextBot.x];
+    if (target === "#") return;
+    if (target === ".") {
+      bot = nextBot;
+      return;
+    }
+
+    let boxesToTest = [{ ...nextBot, char: target }];
+    const boxesToMove = {};
+
+    while (boxesToTest.length) {
+      const nextBoxestoTest = [];
+      for (const box of boxesToTest) {
+        const next = applyDirection(box, direction);
+        const nextChar = map[next.y][next.x];
+        if (nextChar === "#") return;
+        if (nextChar === "[" || nextChar === "]" || nextChar === "O") {
+          nextBoxestoTest.push({ ...next, char: nextChar });
+        }
+        if (
+          (direction === directionCoors.DOWN ||
+            direction === directionCoors.UP) &&
+          box.char !== "O"
+        ) {
+          const otherSideDirection =
+            box.char === "[" ? directionCoors.RIGHT : directionCoors.LEFT;
+          const otherCoors = applyDirection(box, otherSideDirection);
+          const str = nodeToString(otherCoors);
+          if (!boxesToMove[str]) {
+            nextBoxestoTest.push({
+              ...otherCoors,
+              char: map[otherCoors.y][otherCoors.x],
+            });
+          }
+        }
+        const str = nodeToString(box);
+        boxesToMove[str] = { ...box };
+      }
+      boxesToTest = nextBoxestoTest;
+    }
+
+    Object.values(boxesToMove).forEach(({ x, y }) => (map[y][x] = "."));
+    Object.values(boxesToMove).forEach((box) => {
+      const newPos = applyDirection(box, direction);
+      map[newPos.y][newPos.x] = box.char;
+    });
+    bot = nextBot;
+  });
+
+  return isPartTwo ? score(map, "[") : score(map, "O");
+}
+
+function getMap(mapString, isPartTwo) {
+  const mapP1 = to2dArray(mapString);
+  if (!isPartTwo) return mapP1;
+  return mapP1.map((line) =>
+    line.flatMap((element) => {
+      switch (element) {
+        case "#":
+          return ["#", "#"];
+        case "O":
+          return ["[", "]"];
+        case ".":
+          return [".", "."];
+        case "@":
+          return ["@", "."];
+      }
+    })
+  );
+}
+
+function score(map, char) {
+  const charToCoor = createCharToCoor(map);
+  return charToCoor[char]
     .map(({ x, y }) => y * 100 + x)
     .reduce((a, b) => a + b, 0);
-
-  return [partOne, 0];
 }
 
 module.exports = solve;
